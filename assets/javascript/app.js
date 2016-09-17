8e9479d34ab697dc490ac9a1dfb2e89e28def366
Drake Ballard <drakeballard@gmail.com>
2016-09-17 11:18:51 -0500

    adding the firebase application to the js file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
-alert("Test 1,2,3");
+// alert("Test 1,2,3");
+
+//Firebase application
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDjEsXkOlnjKIq2L-Ldgr2SYTvXRWjhgfc",
+  authDomain: "music-underground.firebaseapp.com",
+  databaseURL: "https://music-underground.firebaseio.com",
+  storageBucket: "music-underground.appspot.com",
+  messagingSenderId: "381719161618"
+};
+firebase.initializeApp(config);
