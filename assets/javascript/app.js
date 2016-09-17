fdb1e3d80cde489ef52d6d08fc58ddf18cc2ba5b
Drake Ballard <drakeballard@gmail.com>
2016-09-17 13:29:04 -0500

    adding some Pseudocode for a potential dynamic actions on registrant information

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,4 +13,17 @@
 
 
 //Pseudo code
-//Registrant needs to input information for the following: Artist name, location, song, facebook, and upload song
+//Registrant needs to input information for the following: Artist name, location (autocomplete), song, facebook, and upload song
+
+    //if registrant does not include all field box, error message needs to be alerted
+
+//if info is input correctl, information will be stored in a database
+
+//append information to table and create hyperlinks to open container with artist information
+  // (profile image form facebook will be displaued with other information)
+
+//click on song, and you will be able to play & pause
+
+//click on location, google map api will be generated
+
+// click on facebook redirects to their facebook page
