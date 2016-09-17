44b4850c73b568d5154d23dca1143c48940ceb28
Drake Ballard <drakeballard@gmail.com>
2016-09-17 13:14:25 -0500

    start on pseudo code worked on potential columns. reverted html to original

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,16 @@
 // alert("Test 1,2,3");
 
 //Firebase application
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDjEsXkOlnjKIq2L-Ldgr2SYTvXRWjhgfc",
-  authDomain: "music-underground.firebaseapp.com",
-  databaseURL: "https://music-underground.firebaseio.com",
-  storageBucket: "music-underground.appspot.com",
-  messagingSenderId: "381719161618"
-};
-firebase.initializeApp(config);
+// // Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyDjEsXkOlnjKIq2L-Ldgr2SYTvXRWjhgfc",
+//   authDomain: "music-underground.firebaseapp.com",
+//   databaseURL: "https://music-underground.firebaseio.com",
+//   storageBucket: "music-underground.appspot.com",
+//   messagingSenderId: "381719161618"
+// };
+// firebase.initializeApp(config);
+
+
+//Pseudo code
+//Registrant needs to input information for the following: Artist name, location, song, facebook, and upload song
