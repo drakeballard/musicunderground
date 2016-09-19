011f7f6b0405175aaa3422b55f6f488503e98e34
Drake Ballard <drakeballard@gmail.com>
2016-09-18 21:00:00 -0500

    submit buttons with variables (artist, song, location, fb, console log success)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,30 @@
 //Pseudo code
 //Registrant needs to input information for the following: Artist name, location (autocomplete), song, facebook, and upload song
 
+// Capture Button Click
+$("#addArtist").on("click", function() {
+
+	// Capture User Inputs and store into variables
+	var artist = $('#inputArtist').val().trim();
+	var song = $('#inputSong').val().trim();
+	var location = $('#inputLocation').val().trim();
+	var facebook = $('#inputFB').val().trim();
+
+	// Console log each of the user inputs to confirm you are receiving them
+	console.log(artist);
+	console.log(song);
+	console.log(location);
+	console.log(facebook);
+
+	// Dump all of the new information into the relevant sections "THIS MAY BE REPLACED USING FIREBASE DATABASE FOR HTML REPRESENTATION"
+	// $("#namedisplay").html(name);
+	// $("#emaildisplay").html(email);
+	// $("#agedisplay").html(age);
+	// $("#commentdisplay").html(comment);
+
+	// Don't refresh the page!
+	return false;
+})
 
 
     //if registrant does not include all field box, error message needs to be alerted
