e8b8fb34cd9ecd0673b7ff2d3ed10f97a345b5c0
Drake Ballard <drakeballard@gmail.com>
2016-09-27 21:22:16 -0500

    inital start for timer.js

---
+++ assets/javascript/timer.js
@@ -0,0 +1,8 @@
+// alert("TIMER BITCH");
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log("currentTime " + currentTime);
+var destinationTime = moment("SAT 11:59:00 PM");
+var counter = currentTime.diff(destinationTime, 'd:hh:mm:ss');
+console.log(counter);
+
+// var countdown = moment().endOf(SAT ).fromNow();
