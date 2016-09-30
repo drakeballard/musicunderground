83adf6685131273b2fbb953d96ddf64eeb67ed61
Drake Ballard <drakeballard@gmail.com>
2016-09-29 20:34:24 -0500

    added the jumbotron transparent

--- assets/javascript/timer.js
+++ assets/javascript/timer.js
@@ -1,13 +1,3 @@
-// // alert("TIMER BITCH");
-// //timer function\
-//
-// var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-// console.log("currentTime " + currentTime);
-// var destinationTime = moment("SAT 11:59:00 PM");
-// var counter = currentTime.diff(destinationTime, 'd:hh:mm:ss');
-// console.log(counter);
-//
-// // var countdown = moment().endOf(SAT ).fromNow();
 
 var deadline = 'October 1 2016 23:59:59 GMT-0500';
   // console.log(deadline);
